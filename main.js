6e1e69d7c8efbc8dfe4e4ea6ba24f0e862eb043b
Kchalmers <kevinqchal@yahoo.com>
2017-07-03 02:19:43 -0700

    game is funcional

--- main.js
+++ main.js
@@ -1,8 +1,107 @@
 function Game(){
-    let memory = [];
-    let userClicks = [];
+    let self = this;
+    let pattern = [];
+    let started = false;
+    let strict = false;
+    let steps = 1;
+    let currentColor = 0;
+    let gameButton = $(".gameBtn");
+    let currentPatternColor = 0;
+    gameButton.attr("disabled", true);
+
+    this.init = function () {
+        gameButton.click(this.playerClick);
+        $(".start").click(this.start);
+        $(".restart").click(this.reset);
+        $(".strict").click(this.toggleStrict);
+    };
+    this.start = function () {
+        started = true;
+        self.addToPattern();
+    };
+    this.playPattern = function () {
+        currentPatternColor = 0;
+        for (let i = 0; i < pattern.length; i++) {
+            setTimeout(self.showPattern, 500 * i)
+        }
+        gameButton.attr("disabled", false);
+    };
+    this.showPattern = function () {
+        $(`.${pattern[currentPatternColor]}`).animate({
+            opacity: 0.2
+        }, 300).animate({
+            opacity: 1
+        }, 100);
+        currentPatternColor++;
+    };
+    this.addToPattern = function () {
+        switch (Math.floor(Math.random()*(4-1+1)+1)){
+            case 1:
+                pattern.push("green");
+                break;
+            case 2:
+                pattern.push("red");
+                break;
+            case 3:
+                pattern.push("yellow");
+                break;
+            case 4:
+                pattern.push("blue");
+                break;
+        }
+        this.playPattern();
+    };
+    this.reset = function () {
+        setTimeout(self.clearStatus, 3000);
+        gameButton.attr("disabled", true);
+        currentColor = 0;
+        steps = 1;
+        started = false;
+        pattern = [];
+        $(".level").empty().append(`Level: ${steps}`);
+    };
+    this.clearStatus = function () {
+        $(".status").empty()
+    };
+    this.toggleStrict = function () {
+        if(strict){
+            return strict = false;
+        }
+        return strict = true;
+    };
+    this.playerClick = function () {
+        if(started) {
+            if (this.classList["0"] === pattern[currentColor]) {
+                self.victoryCheck();
+            }
+            else if (strict) {
+                $(".status").append("Defeat!");
+                self.reset();
+            }
+            else if(this.classList["0"] !== pattern[currentColor]){
+                self.playPattern();
+            }
+        }
+    };
+    this.victoryCheck = function () {
+        if(steps === 20){
+            $(".status").append("Victory!");
+            self.reset();
+        }
+        else if(currentColor === pattern.length -1){
+            gameButton.attr("disabled", true);
+            currentColor = 0;
+            steps++;
+            $(".level").empty().append(`Level: ${steps}`);
+            self.addToPattern();
+        }
+        else {
+            currentColor++;
+        }
+    };
 }
 
 $(document).ready(function () {
-
+    const start = new Game();
+    start.init();
 });
